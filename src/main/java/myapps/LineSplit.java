f7856132d4d8d9af5aca0c374e80937f04c98907
System Administrator <root@zhkdeMBP.lan>
2018-06-09 23:33:07 +0800

    pipe

--- src/main/java/myapps/LineSplit.java
+++ src/main/java/myapps/LineSplit.java
@@ -10,13 +10,13 @@ import org.apache.kafka.streams.kstream.KStream;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-public class Pipe{
+public class LineSplit {
 
 
 	public static void main(String[] args)throws Exception{
 
         Properties properties=new Properties();
-        properties.put(StreamsConfig.APPLICATION_ID_CONFIG,"streams-pipe");
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG,"streams-linesplit");
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass());
